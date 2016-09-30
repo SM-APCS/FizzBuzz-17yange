5f06e7ffd1f58e94a608bbcb85a6859abaacccbd
17yange <17yange@smtexas.org>
2016-09-30 13:46:25 -0500

    Woohoo

--- FizzBuzz/src/fizzbuzz/FizzBuzz.java
+++ FizzBuzz/src/fizzbuzz/FizzBuzz.java
@@ -15,8 +15,26 @@ public class FizzBuzz {
     /**
      * @param args the command line arguments
      */
-    public static void main(String[] args) {
-        // TODO code application logic here
+    public static void main(String[] args) 
+    {
+        for(int i = 1; i < 101; i++)
+        {
+            if(i % 15 == 0)
+            {
+                System.out.println("FizzBuzz");
+            }
+            else if(i % 3 == 0)
+            {
+                System.out.println("Fizz");
+            }
+            else if(i % 5 == 0)
+            {
+                System.out.println("Buzz");
+            }
+            else
+            {
+                System.out.println(i);  
+            }
+        }
     }
-    
 }
