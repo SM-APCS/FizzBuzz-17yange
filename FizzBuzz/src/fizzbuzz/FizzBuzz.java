b942874bd98592fa0e750750a11fa6712d6fc727
17yange <17yange@smtexas.org>
2016-09-30 13:49:44 -0500

    Woot

--- FizzBuzz/src/fizzbuzz/FizzBuzz.java
+++ FizzBuzz/src/fizzbuzz/FizzBuzz.java
@@ -7,13 +7,16 @@ package fizzbuzz;
 
 /**
  *
- * @author Mr. T - CHANGE TO YOUR NAME
+ * @author Eddie Yang - CHANGE TO YOUR NAME
  * @class AP CSA
  */
 public class FizzBuzz {
 
     /**
-     * @param args the command line arguments
+     * @param program checks to see if the number is divisible by 15 first.
+     * @param If it is it displays Fizzbuzz then repeats the for loop for the next number
+     * @param then it checks to see if divisible by 3 or 5
+     * @param if it is then it displays Fizz or Buzz respectively and repeats the for loop until it stops at 100
      */
     public static void main(String[] args) 
     {
